c6e4746f8a078a8f23750f4a13f2a6bd6638d584
PleD1k <pledik.moy@gmail.com>
2025-07-18 09:54:13 +0300

    update schemesJson

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -1,60 +1,67 @@
-const axios = require('../utils/axios.config');
-const endpoints = require('../utils/endpoints');
-const testData = require('../test-data/users.data');
+const axios = require("../utils/axios.config");
+const endpoints = require("../utils/endpoints");
+const testData = require("../test-data/users.data");
 
-describe('Users API', () => {
+const Ajv = require("ajv");
+const addFormats = require("ajv-formats");
+
+const usersListSchema = require("../schemes/users-list.json");
+const userSingleSchema = require("../schemes/user-single.json");
+const userCreateSchema = require("../schemes/user-create.json");
+const userUpdateSchema = require("../schemes/user-update.json");
+
+const ajv = new Ajv();
+addFormats(ajv);
+
+describe("Users API", () => {
   beforeAll(() => {
-    if (!process.env.BASE_URL) {
-      throw new Error('BASE_URL is not found');
-    }
+    if (!process.env.BASE_URL) throw new Error("BASE_URL is not found");
   });
 
-  test('GET users should return list of users', async () => {
+  test("GET users should return list of users", async () => {
+    const validate = ajv.compile(usersListSchema);
     const response = await axios.get(endpoints.users.list);
     expect(response.status).toBe(200);
-    expect(response.data).toHaveProperty('data');
-    expect(Array.isArray(response.data.data)).toBe(true);
-    expect(response.data.data.length).toBeGreaterThan(0);
-
-    const user = response.data.data[0];
-    expect(user).toHaveProperty('id');
-    expect(user).toHaveProperty('email');
-    expect(user).toHaveProperty('first_name');
-    expect(user).toHaveProperty('last_name');
+    expect(validate(response.data)).toBe(true);
   });
 
-  test('GET users/7 should return specific user', async () => {
+  test("GET users/7 should return specific user", async () => {
+    const validate = ajv.compile(userSingleSchema);
     const response = await axios.get(endpoints.users.single(7));
-
     expect(response.status).toBe(200);
-    expect(response.data.data).toHaveProperty('id', 7);
-    expect(response.data.data).toHaveProperty('email');
-    expect(response.data.data).toHaveProperty('first_name');
-    expect(response.data.data).toHaveProperty('last_name');
+    const valid = validate(response.data);
+    expect(valid).toBe(true);
+    if (!valid) console.log(validate.errors);
+    expect(response.data.data.id).toBe(7);
   });
 
-  test('POST users should create a user', async () => {
+  test("POST users should create a user", async () => {
+    const validate = ajv.compile(userCreateSchema);
     const response = await axios.post(endpoints.users.create, testData.newUser);
-
     expect(response.status).toBe(201);
-    expect(response.data).toHaveProperty('id');
+    const valid = validate(response.data);
+    expect(valid).toBe(true);
+    if (!valid) console.log(validate.errors);
     expect(response.data.name).toBe(testData.newUser.name);
     expect(response.data.job).toBe(testData.newUser.job);
-    expect(response.data).toHaveProperty('createdAt');
   });
 
-  test('PUT users/7 should update a user 7', async () => {
-    const response = await axios.put(endpoints.users.update(7), testData.updatedUser);
-
+  test("PUT users/7 should update a user 7", async () => {
+    const validate = ajv.compile(userUpdateSchema);
+    const response = await axios.put(
+      endpoints.users.update(7),
+      testData.updatedUser
+    );
     expect(response.status).toBe(200);
+    const valid = validate(response.data);
+    expect(valid).toBe(true);
+    if (!valid) console.log(validate.errors);
     expect(response.data.name).toBe(testData.updatedUser.name);
     expect(response.data.job).toBe(testData.updatedUser.job);
-    expect(response.data).toHaveProperty('updatedAt');
   });
 
-  test('DELETE users/7 should delete a user 7', async () => {
+  test("DELETE users/7 should delete a user 7", async () => {
     const response = await axios.delete(endpoints.users.delete(7));
-
     expect(response.status).toBe(204);
   });
 });
